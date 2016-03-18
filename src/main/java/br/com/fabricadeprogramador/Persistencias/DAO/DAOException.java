1877e14ede8f2c541031708b2de0d5ad93fc543a
MattheusSantana <matheusess@hotmail.com>
2016-03-17 21:12:28 -0300

    Adicionando Exceptions dos DAOs

--- src/main/java/br/com/fabricadeprogramador/Persistencias/DAO/DAOException.java
+++ src/main/java/br/com/fabricadeprogramador/Persistencias/DAO/DAOException.java
@@ -3,5 +3,8 @@ package br.com.fabricadeprogramador.Persistencias.DAO;
 /**
  * Created by Matheus on 17/03/2016.
  */
-public class DAOException {
+public class DAOException extends Throwable {
+    public DAOException(String msg) {
+        super(msg);
+    }
 }
